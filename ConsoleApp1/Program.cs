669c11f9d5fb378fd508dbeb5f54209bc3b836e1
Konstantin Smirnov <sasharugby@yandex.ru>
2024-05-30 18:30:13 +0300

    12done

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+int a = ReadInt("введите число");
+Console.Write("введите знак");
+string b = Console.ReadLine();
+int c = ReadInt("введите число");
+double g = 0;
+if(b == "+")
+{
+    g = a + c;
+    Console.WriteLine($"{a} + {c} = {g}");
+}
+
+if (b == "-")
+{
+    g = a - c;
+    Console.WriteLine($"{a} - {c} = {g}");
+}
+if (b == "*")
+{
+    g = a * c;
+    Console.WriteLine($"{a} * {c} = {g}");
+}
+if (b == "/")
+{
+    g = a / c;
+    Console.WriteLine($"{a} / {c} = {g}");
+}
